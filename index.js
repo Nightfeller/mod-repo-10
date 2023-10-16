d7a3de2fb950f1e85a9c6afe3483414b8406232e
Nightfeller <family2earls@gmail.com>
2023-10-16 13:50:05 -0500

    almost finished with code. README needs usage.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const inquirer = require('inquirer');
 const { writeFile } = require('fs/promises');
-const chalk = require('chalk');
-//const chalk = new normalChalk({level: 3});
+// const chalk = require('chalk');
+const shap = require('./lib/shapes.js');
+// console.log(shap);
+// const chalk = new normalChalk({level: 3});
+
+class Insert {
+    constructor(inp, li) {
+        this.one = inp,
+        this.two = li
+    }
+
+    messageLimiter(t) {
+        if (t.length <= 3) {
+            return true;
+        }
+    }
+}
+
+
+const load = new Insert('input', 'list');
+const nORh = "Put in a broad color name or a hexadecimal code.";
+
 
 const inputs = [
     {
@@ -15,7 +35,7 @@ const inputs = [
         }
     },
     {
-        type: 'list',
+        type: load.two,
         message: 'What is the shape of the logo?',
         name: 'shapeName',
         choices: [
@@ -25,52 +45,45 @@ const inputs = [
         ]
     },
     {
-        type: 'list',
-        message: 'Do you want to use a broad color name or a hexadecimal?',
-        name: 'colorType',
-        choices: [
-            'Hexadecimal',
-            'Broad Color Name'
-        ]
-    },
-    {
-        type: 'input',
-        message: 'What color do you want your initials to be?',
+        type: load.one,
+        message: `What color do you want your initials to be? ${nORh}`,
         name: 'initialColors'
     },
     {
-        type: 'input',
-        message: 'What color do you want the logo shape to be?',
+        type: load.one,
+        message: `What color do you want the logo shape to be? ${nORh}`,
         name: 'shapeColors'
     }
 
 ];
 
 inquirer.prompt(inputs).then((response) => {
-    const iName = response.initialsName;
-    const sName = response.shapeName;
-    const iC = response.initialColors;
-    const sC = response.shapeColors;
-    let color;
-    let paint;
 
-    if (response.colorType == 'Hexadecimal') {
-        color = chalk.hex(iC);
-        paint = chalk.hex(sC);
-        console.log(chalk.hex);
-    } else {
-        color = chalk[iC];
-        paint = chalk[sC];
-    }
+    // const shape = new shap.Shapes(response.initialColors, response.shapeColors);
+    // const iName = response.initialsName;
+    // const sName = response.shapeName;
+    // const iC = response.initialColors;
+    // const sC = response.shapeColors;
+    // let color;
+    // let paint;
+    // console.log(shape);
+    // if (response.colorType == 'Hexadecimal') {
+    //     color = chalk.hex(iC);
+    //     paint = chalk.hex(sC);
+    //     console.log(chalk.hex);
+    // } else {
+    //     color = chalk[iC];
+    //     paint = chalk[sC];
+    // }
 
-    console.log(color);
-    console.log(paint);
-    console.log(iC);
-    console.log(sC);
-    console.log(color(iName)); 
-    console.log(paint(sName));
+    // console.log(color);
+    // console.log(paint);
+    // console.log(response.initialColors);
+    // console.log(sC);
+    // console.log(color(iName)); 
+    // console.log(paint(sName));
 
-    writeFile('logo.svg', renderHTML(response, paint, color));
+    writeFile('logo.svg', shap.renderHTML(response));
 }).catch(err => {
     console.log(err);
 });
