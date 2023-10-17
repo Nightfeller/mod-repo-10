1c24e101fe83ed42ffb4c7a679cd2f330db7e611
Nightfeller <family2earls@gmail.com>
2023-10-16 20:30:20 -0500

    Finished with code. Tests don't work. README.md is finished.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# mod-repo-10
-Repository for Module 10 challenge.
+# Logo Generator
+
+## Description
+
+This is a logo generator for my Module 10 Challenge. Its function is to create simple logos consisting of three types of shapes (Square, Circle, and Triangle), and a three-lettered name for your logo. For coloring, you can choose between broad color names, or a hexadecimal code.
+
+## Installation
+
+You will need Inquirer and Jest for this program. In the terminal, type "npm i inquirer@8.2.4" and "npm i jest@29.7.0" to install the required packages. If you ever find a problem, follow the terminal's directions to fix them. It should tell you what commands you have to type in.
+You could also simply use "npm install" or "npm i" to download any of the
+
+## Usage
+
+In the terminal, type in "node index.js" and begin the process.
+
+First type in the three initials of the logo. If you type more than three characters, you won't be able to save the logo initials.
+
+After typing the initials, you can select the three options of shapes: Square, Circle, or Triangle.
+
+Afterwards, you have to say the color you want on your logo's initials and shape. You can choose either a broad color name, or a hexadecimal color code. Note that if you follow the broad naming scheme, the code uses the CSS/HTML coloring scheme.
+
+## Credits
+
+There is no one else who contrbuted to this.
+
+## License
+
+There is no license on this repository.
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const inquirer = require('inquirer');
 const { writeFile } = require('fs/promises');
-// const chalk = require('chalk');
+
 const shap = require('./lib/shapes.js');
-// console.log(shap);
-// const chalk = new normalChalk({level: 3});
 
 class Insert {
     constructor(inp, li) {
@@ -11,11 +9,13 @@ class Insert {
         this.two = li
     }
 
-    messageLimiter(t) {
-        if (t.length <= 3) {
-            return true;
-        }
-    }
+    //Might come back to make this work.
+
+    // messageLimiter(t) {
+    //     if (t.length <= 3) {
+    //         return true;
+    //     }
+    // }
 }
 
 
@@ -25,7 +25,7 @@ const nORh = "Put in a broad color name or a hexadecimal code.";
 
 const inputs = [
     {
-        type: 'input',
+        type: load.one,
         message: 'What are the initials for the logo?',
         name: 'initialsName',
         validate: (input) => {
@@ -58,32 +58,10 @@ const inputs = [
 ];
 
 inquirer.prompt(inputs).then((response) => {
-
-    // const shape = new shap.Shapes(response.initialColors, response.shapeColors);
-    // const iName = response.initialsName;
-    // const sName = response.shapeName;
-    // const iC = response.initialColors;
-    // const sC = response.shapeColors;
-    // let color;
-    // let paint;
-    // console.log(shape);
-    // if (response.colorType == 'Hexadecimal') {
-    //     color = chalk.hex(iC);
-    //     paint = chalk.hex(sC);
-    //     console.log(chalk.hex);
-    // } else {
-    //     color = chalk[iC];
-    //     paint = chalk[sC];
-    // }
-
-    // console.log(color);
-    // console.log(paint);
-    // console.log(response.initialColors);
-    // console.log(sC);
-    // console.log(color(iName)); 
-    // console.log(paint(sName));
-
     writeFile('logo.svg', shap.renderHTML(response));
 }).catch(err => {
     console.log(err);
 });
+
+
+// lol
